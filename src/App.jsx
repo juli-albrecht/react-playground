5aedb62dfa8868b919bf0a1aff82be4c4e32de69
Juli Albrecht <juliella2002@gmail.com>
2025-11-01 13:32:33 +0100

    add app, login and welcomemessage component and logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,49 @@
+import { useState } from "react";
 
+const USERS = [{ name: "Lukas" }, { name: "Juli" }, { name: "Roxy" }];
 
 function App() {
+  const [user, setUser] = useState();
+  if (user !== undefined) {
+    return <WelcomeMessage name={user.name} />;
+  }
+  return (
+    <>
+      <Login setUser={setUser} />
+    </>
+  );
+}
 
+export default App;
+
+function Login({ setUser }) {
+  const [input, setInput] = useState("");
 
   return (
     <>
-
+      <h1>You are not logged in!</h1>
+      <input
+        type="text"
+        onChange={(event) => {
+          setInput(event.target.value);
+        }}
+      />
+      <button
+        onClick={() => {
+          const filteredUserByName = USERS.filter((user) => {
+            return user.name === input;
+          });
+          if (filteredUserByName.length === 1) {
+            setUser(filteredUserByName[0]);
+          }
+        }}
+      >
+        Login
+      </button>
     </>
-  )
+  );
 }
 
-export default App
+function WelcomeMessage({ name }) {
+  return <h1>Hello {name}!</h1>;
+}
